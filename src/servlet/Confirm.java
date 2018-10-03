a8cd0af4a71d0b286ca5a6bf945beda370d04d6c
ASUS <ASUS@ASUS>
2018-10-03 21:25:03 +0800

    haha

---
+++ src/servlet/Confirm.java
@@ -0,0 +1,51 @@
+package servlet;
+
+import java.io.IOException;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.DBConnection;
+
+/**
+ * Servlet implementation class Confirm
+ */
+@WebServlet("/Confirm")
+public class Confirm extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Confirm() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String id=request.getParameter("id");
+		String sql1 = null;
+		sql1="update SignIn set comfirm='"+"true"+"' where SignNo ="+Integer.valueOf(id);
+		DBConnection db1=new DBConnection();
+		db1.getCon();
+		db1.doPstm(sql1);
+		db1.closed();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
